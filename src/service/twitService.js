e5904e587610dcaa6d60d00b10978355cd4efa8f
Baldi <baldilp@gmail.com>
2018-09-02 15:29:54 -0300

    add feature to move uploaded images to posted folder (so photos won't repeat)

--- src/service/twitService.js
+++ src/service/twitService.js
@@ -10,22 +10,40 @@ const getRandomImage = require('../util/getRandomImage')
 const T = new Twit(config)
 
 const uploadRandomImage = (images) => {
-  console.log('[1/4] - Selecionando uma imagem aleatóriamente supimpa')
+  if (images.length) {
+    console.log('[1/4] - Selecionando uma imagem aleatóriamente supimpa')
 
-  const imgPath = path.join(__dirname, '..', `/images/new/${getRandomImage(images)}`)
-  const b64content = fs.readFileSync(imgPath, { encoding: 'base64' })
+    const imageToPost = getRandomImage(images)
+    const imgPath = path.join(__dirname, '..', `/images/museum/${imageToPost}`)
+    const b64content = fs.readFileSync(imgPath, { encoding: 'base64' })
 
-  console.log('[2/4] - Fazendo upload da imagem para o museu')
+    console.log('[2/4] - Fazendo upload da imagem para o museu')
 
-  T.post('media/upload', { media_data: b64content })
-    .then(({ data: uploadData }) => {
-      console.log('[3/4] - Upload relizado com sucesso, agora é só mandar o tweet...')
+    T.post('media/upload', { media_data: b64content })
+      .then(({ data: uploadData }) => {
+        console.log('[3/4] - Upload relizado com sucesso, agora é só mandar o tweet...')
 
-      T.post('statuses/update', { media_ids: new Array(uploadData.media_id_string) })
-        .then(tweetData => {
-          console.log('[4/4] - Tweet sapecado com sucesso, damn nigga')
-        })
-    })
+        T.post('statuses/update', { media_ids: new Array(uploadData.media_id_string) })
+          .then(tweetData => {
+            console.log('[4/4] - Tweet sapecado com sucesso, damn nigga')
+            moveUploadedImage(imageToPost)
+          })
+      })
+  } else {
+    console.log('Opa, tá na hora de adicionar mais fotos ao acervo bro');
+  }
+}
+
+const moveUploadedImage = (imageToPost) => {
+  const imagesFolderPath = path.join(__dirname, '..', '/images')
+  const museumImgPath = `${imagesFolderPath}/museum/${imageToPost}`
+  const postedImgPath = `${imagesFolderPath}/posted/${imageToPost}`
+
+  fs.rename(museumImgPath, postedImgPath, (err) => {
+    if (err) {
+      console.log('Ocorreu um erro ao mover a imagem postada para pasta posted, dá uma olhada nisso ae plz')
+    }
+  })
 }
 
 module.exports = { uploadRandomImage }
